53f7d7ce2583d5565bcb5b3bb5102e9a32734309
victornavareno <victorandresnm@gmail.com>
2023-06-13 10:48:01 +0200

    FORMAT CHANGES SONDEO

--- sondeo.cpp
+++ sondeo.cpp
@@ -4,7 +4,7 @@
 void mostrarMenuConfirmacion()
 {
     cout << endl;
-    cout << "Acepta el cierre de la comunicacion:" << endl;
+    cout << "¿Acepta el cierre de la comunicacion?" << endl;
     cout << "[1] Si" << endl;
     cout << "[2] No" << endl;
 }
@@ -16,8 +16,8 @@ void MaestroSondeo(interface_t *interfaz, unsigned char mac_origen[6], unsigned 
     unsigned char control;
     unsigned char numeroTrama;
 
-    direccion = 'T'; // DIRECCION: 'T' PORQUE ES OPERACION DE SONDEO
-    control = 5; // CONTORL: 5 PORQUE ES TRAMA DE CONTROL ENQ
+    direccion = 'T';   // DIRECCION: 'T' PORQUE ES OPERACION DE SONDEO
+    control = 5;       // CONTORL: 5 PORQUE ES TRAMA DE CONTROL ENQ
     numeroTrama = '0'; // OSCILA ENTRE 0 Y 1, PRIMERA SIEMPRE 0
 
     EnviarTramaControl(interfaz, mac_origen, mac_destino, tipo, direccion, control, numeroTrama); // ENVIAMOS Y MOSTRAMOS TRAMA DE CONTROL ENQ
@@ -55,54 +55,53 @@ void MaestroSondeo(interface_t *interfaz, unsigned char mac_origen[6], unsigned 
                 unsigned char teclaPulsada;
                 teclaPulsada = getch();
 
-                switch (teclaPulsada)
+                // PULSACION DE 2: CONTINUAMOS CON LA COMUNICACION ENVIANDO TRAMA ACK
+                if (teclaPulsada == 2)
                 {
-                case '1':
-                    control = 6;
-                    fin = true;
-                    // MOSTRAREMOS LA TRAMA ENVIADA ANTES DE SALIR DEL METODO
-                    break;
-
-                case '2':
+                    // ENVIAMOS TRAMA NACK (21) Y SEGUIMOS CON LA COMUNICACION
                     EnviarTramaControl(interfaz, mac_origen, mac_destino, tipo, direccion, 21, numeroTrama);
                     MostrarTrama('E', direccion, control, numeroTrama, ' ');
                     cout << endl;
 
-                    // Invertimos numeroTrama para pasar a la siguiente:
+                    // SWAP numeroTrama
                     if (numeroTrama == '0')
+                    {
                         numeroTrama = '1';
+                    }
                     else
                         numeroTrama = '0';
+                }
 
-                default:
-                    cout << "Tecla inválida, cerrando la comunicación..." << endl;
+                // PULSACION DE 1 O TECLA ERRONEA - CERRAMOS LA COMUNICACION
+                else
+                {
+                    cout << "Cerrando comunicacion..." << endl;
                     control = 6;
                     fin = true;
-                    break;
+                    // MOSTRAREMOS LA TRAMA ENVIADA ANTES DE SALIR DEL METODO
                 }
             }
         }
+
         else
         {
             if (longitud != 0 && flujoOutput.is_open())
             {
-                flujoOutput.write(cadena, longitud);
+                flujoOutput.write(cadena, longitud); // ESCRIBIMOS EN FICHERO
                 control = 6;
             }
             else
             {
-                control = 21;
+                control = 21; // PASARA A SER TRAMA NACK
             }
         }
 
-        // ENVIAMOS Y MOSTRAMOS LA TRAMA CONTROL DE RESPUESTA ACK
+        // ENVIAMOS Y MOSTRAMOS LA TRAMA FINAL CONTROL DE RESPUESTA ACK
         EnviarTramaControl(interfaz, mac_origen, mac_destino, tipo, direccion, control, numeroTrama);
         MostrarTrama('E', direccion, control, numeroTrama, ' ');
-
     }
 
     flujoOutput.close();
-
     cout << endl
          << "Fin de sondeo por parte del Maestro" << endl;
 }
@@ -123,11 +122,13 @@ void EsclavoSondeo(interface_t *interfaz, unsigned char mac_origen[6], unsigned 
     // EJECUTAMOS MIENTRAS NO RECIBAMOS UN ACK - CONTROL 6 (CONFIRMACION)
     while (control != 6)
     {
-        EnviarTramaControl(interfaz, mac_origen, mac_destino, tipo, direccion, 4, numTramaEnviada); // CONTROL 4: EOT
+        // ENVIAMOS LA TRAMA EOT (4)
+        EnviarTramaControl(interfaz, mac_origen, mac_destino, tipo, direccion, 4, numTramaEnviada);
 
-        RecibirTramaControl(interfaz, direccion, control, numeroTrama); // RECIBIMOS TRAMAS HASTA RECIBIR CONTROL 6:  ACK - CONFIRMACION
+        // RECIBIMOS LA TRAMA DE RESPUESTA - CONFIRMACION ACK (6)
+        RecibirTramaControl(interfaz, direccion, control, numeroTrama); 
 
-        if (control == 21 || control == 6) // 21: NACK - 6: ACK
+        if (control == 21 || control == 6) // SI RECIBIMOS -> 21: NACK || 6: ACK
         {
             MostrarTrama('R', direccion, control, numeroTrama, ' ');
             cout << endl;
@@ -137,7 +138,7 @@ void EsclavoSondeo(interface_t *interfaz, unsigned char mac_origen[6], unsigned 
         {
             numTramaEnviada = '1';
         }
-        else if (numTramaEnviada == '1')
+        else 
         {
             numTramaEnviada = '0';
         }
